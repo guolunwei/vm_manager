fb2a3f38a23a214c2f644e9f8ebe6617897d8ba5
Lunwei Guo <Lunwei.guo@hotmail.com>
2025-06-24 23:05:18 +0800

    dos2unix

--- set_env.sh
+++ set_env.sh
@@ -7,10 +7,10 @@ set -e
 SCRIPT_DIR=$(realpath "$(dirname "$0")")
 echo ${SCRIPT_DIR}
 
+mkdir -p /etc/profile.d
 cat > /etc/profile.d/vmctl.sh <<EOF
 export PATH=\$PATH:$SCRIPT_DIR
 alias vm="vmctl.sh"
 EOF
 
 cp vmctl-completion.sh /etc/profile.d/
-source /etc/profile
